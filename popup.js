365bfef4276536b921d067ae83c4336cb90324db
p4telj <pateljim@umich.edu>
2019-05-08 22:43:01 -0400

    edited popup html and js

--- popup.js
+++ popup.js
@@ -13,3 +13,21 @@
 //           {code: 'document.body.style.backgroundColor = "' + color + '";'});
 //     });
 //   };
+
+// this one shouldn't programatically inject a script, that will be done declaratively
+// but this should be able to communicate with it
+
+// $(function() {
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//         chrome.tabs.executeScript(
+//             tabs[0].id,
+//             {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//     });
+// });
+
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.executeScript(tabs[0].id, { file: "jquery-3.4.1.min.js" }, function() {
+//         chrome.tabs.executeScript(null, { file: "hulu.js" });
+//     });
+// });
+
